355287692133272f3d2ad47c934acbe648ad13e7
adk <9088825345@qq.com>
2017-11-27 13:38:18 +0800

    后台模板基础布局

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -4,7 +4,24 @@ namespace app\admin\controller;
 class Index extends Common{
     public function index(){
 
-        return $this->fetch();
+        $d = db('wxt')->select();
+        
+        $n = '';
+        $u = '';
+        $o = '';
+        $t = '';
+
+        foreach($d as $v){
+            $n .= $v['name'].',';
+            $u .= $v['unionid'].',';
+            $o .= $v['openid'].',';
+            $t .= $v['tel'].',';
+        }
+
+        dump(trim($n,','));
+        dump(trim($u,','));
+        dump(trim($o,','));
+        dump(trim($t,','));
     }
 
     public function test(){
